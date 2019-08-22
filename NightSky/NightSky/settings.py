dc3a58389c10fee880fea2f355276c31855e7845
baruna0330 <bolting0330@naver.com>
2019-08-22 22:15:08 +0900

    4rd Commit

--- NightSky/NightSky/settings.py
+++ NightSky/NightSky/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [django-env.sespktixmp.us-west-2.elasticbeanstalk.com]
 
 
 # Application definition
